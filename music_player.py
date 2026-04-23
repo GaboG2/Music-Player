34e563560fee9e5db534d1c94f1c98cb50b5977d
Gabriel D. <gaboale166@gmail.com>
2026-04-23 16:40:49 -0400

    audio controls added to the page in a row

--- music_player.py
+++ music_player.py
@@ -18,5 +18,11 @@ def main(page: ft.Page):
     next_song_button = ft.Button(content = "Next", on_click = next_song)
     previous_song_button = ft.Button(content = "Previous", on_click = previous_song)
 
+    page.add(
+        ft.Row(
+            controls = [play_pause_button, next_song_button, previous_song_button],
+            alignment = page.horizontal_alignment
+        )
+    )
 
 ft.run(main = main, assets_dir = "assets")
