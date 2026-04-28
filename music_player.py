9b2cadf867a21c8760932568024d28ed994aec4d
Gabriel D. <gaboale166@gmail.com>
2026-04-28 16:52:05 -0400

    music variable added, does nothing

--- music_player.py
+++ music_player.py
@@ -13,7 +13,8 @@ def main(page: ft.Page):
 
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-
+    
+    music = fta.Audio()
     play_pause_button = ft.Button(content = "Play", on_click = play_pause)
     next_song_button = ft.Button(content = "Next", on_click = next_song)
     previous_song_button = ft.Button(content = "Previous", on_click = previous_song)
