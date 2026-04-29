4d0ad7a9fdceb482e687bd16542ad3351d918617
Gabriel D. <gaboale166@gmail.com>
2026-04-29 17:21:21 -0400

    switch songs now, track info
    i dont know why i keep forgetting to update frequently

--- music_player.py
+++ music_player.py
@@ -7,8 +7,9 @@ builtin_tracks = [
     {"name": "Sphere", "src": "assets/audio/Sphere.mp3"},
     {"name": "These Mistakes are Mine Alone", "src": "assets/audio/These Mistakes are Mine Alone.mp3"},
 ]
-
 def main(page: ft.Page):
+    track_number = 0
+    
     async def play(e):
         await music.play()
     
@@ -18,23 +19,43 @@ def main(page: ft.Page):
     async def resume(e):
         await music.resume()
     
-    async def next_song(e):
-        pass
+    def next_song(e):
+        nonlocal track_number
 
-    async def previous_song(e):
-        pass
+        if track_number == len(builtin_tracks) - 1:
+            track_number = 0
+        else:
+            track_number += 1
+        
+        music.src = builtin_tracks[track_number]["src"]
+        track_info_text.value = f"Track {track_number + 1}: {builtin_tracks[track_number]["name"]}"
+
+    def previous_song(e):
+        nonlocal track_number
+
+        if track_number == 0:
+            track_number = len(builtin_tracks) - 1
+        else:
+            track_number -= 1
+        
+        music.src = builtin_tracks[track_number]["src"]
+        track_info_text.value = f"Track {track_number + 1}: {builtin_tracks[track_number]["name"]}"
 
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     
-    music = fta.Audio(src = builtin_tracks[0]["src"])
+    music = fta.Audio(src = builtin_tracks[track_number]["src"])
+
     play_button = ft.Button(content = "Play", on_click = play)
     pause_button = ft.Button(content = "Pause", on_click = pause)
-    resume_button = ft.Button(content = "resume", on_click = resume)
+    resume_button = ft.Button(content = "Resume", on_click = resume)
     next_song_button = ft.Button(content = "Next", on_click = next_song)
     previous_song_button = ft.Button(content = "Previous", on_click = previous_song)
 
+    track_info_text = ft.Text(f"Track {track_number + 1}: {builtin_tracks[track_number]["name"]}")
+
     page.add(
+        track_info_text,
         ft.Row(
             controls = [previous_song_button, play_button, resume_button, pause_button, next_song_button],
             alignment = page.horizontal_alignment
