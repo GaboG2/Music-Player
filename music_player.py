15ef5101d90d89bedada1b653aa595f0d12f5251
Gabriel D. <gaboale166@gmail.com>
2026-04-29 17:47:51 -0400

    artist values added to tracks

--- music_player.py
+++ music_player.py
@@ -2,11 +2,31 @@ import flet as ft
 import flet_audio as fta
 
 builtin_tracks = [
-    {"name": "Ifertehno", "src": "assets/audio/Ifertehno.mp3"},
-    {"name": "Operation Evolution", "src": "assets/audio/Operation Evolution.mp3"},
-    {"name": "Sphere", "src": "assets/audio/Sphere.mp3"},
-    {"name": "Surface", "src": "assets/audio/Surface.mp3"},
-    {"name": "These Mistakes are Mine Alone", "src": "assets/audio/These Mistakes are Mine Alone.mp3"}
+    {
+        "title": "Ifertehno", 
+        "artist": "Shinrog", 
+        "src": "assets/audio/Ifertehno.mp3"
+    },
+    {
+        "title": "Operation Evolution", 
+        "artist": "Dimrain47", 
+        "src": "assets/audio/Operation Evolution.mp3"
+    },
+    {
+        "title": "Sphere", 
+        "artist": "Creo", 
+        "src": "assets/audio/Sphere.mp3"
+    },
+    {
+        "title": "Surface", 
+        "artist": "Dimrain47", 
+        "src": "assets/audio/Surface.mp3"
+    },
+    {
+        "title": "These Mistakes are Mine Alone", 
+        "artist": "nubbiownz", 
+        "src": "assets/audio/These Mistakes are Mine Alone.mp3"
+    }
 ]
 def main(page: ft.Page):
     track_number = 0
@@ -30,7 +50,7 @@ def main(page: ft.Page):
             track_number += 1
         
         music.src = builtin_tracks[track_number]["src"]
-        track_info_text.value = f"Track {track_number + 1}: {builtin_tracks[track_number]["name"]}"
+        track_info_text.value = f'Track {track_number + 1}: "{builtin_tracks[track_number]["title"]}" from {builtin_tracks[track_number]["artist"]}'
 
     def previous_song(e):
         nonlocal track_number
@@ -41,7 +61,7 @@ def main(page: ft.Page):
             track_number -= 1
         
         music.src = builtin_tracks[track_number]["src"]
-        track_info_text.value = f"Track {track_number + 1}: {builtin_tracks[track_number]["name"]}"
+        track_info_text.value = f'Track {track_number + 1}: "{builtin_tracks[track_number]["title"]}" from {builtin_tracks[track_number]["artist"]}'
 
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
@@ -54,7 +74,7 @@ def main(page: ft.Page):
     next_song_button = ft.Button(content = "▶I", on_click = next_song)
     previous_song_button = ft.Button(content = "I◀", on_click = previous_song)
 
-    track_info_text = ft.Text(f"Track {track_number + 1}: {builtin_tracks[track_number]["name"]}")
+    track_info_text = ft.Text(f'Track {track_number + 1}: "{builtin_tracks[track_number]["title"]}" from {builtin_tracks[track_number]["artist"]}')
 
     page.add(
         track_info_text,
