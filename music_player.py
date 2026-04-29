a100f1b2d50fbb255292c934c6feefaa80477fde
Gabriel D. <gaboale166@gmail.com>
2026-04-29 16:47:53 -0400

    fixed some stuff + new track
    builtin tracks being implemented, music plays now, not much else

--- music_player.py
+++ music_player.py
@@ -1,28 +1,42 @@
 import flet as ft
 import flet_audio as fta
 
+builtin_tracks = [
+    {"name": "Ifertehno", "source": "assets/audio/Ifertehno.mp3"},
+    {"name": "Operation Evolution", "source": "assets/audio/Operation Evolution.mp3"},
+    {"name": "Sphere", "source": "assets/audio/Sphere.mp3"},
+    {"name": "These Mistakes are Mine Alone", "source": "assets/audio/These Mistakes are Mine Alone.mp3"},
+]
+
 def main(page: ft.Page):
-    def play_pause(e):
-        if True:
-            pass
+    async def play(e):
+        await music.play()
     
-    def next_song(e):
+    async def pause(e):
+        await music.pause()
+    
+    async def resume(e):
+        await music.resume()
+    
+    async def next_song(e):
         pass
 
-    def previous_song(e):
+    async def previous_song(e):
         pass
 
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     
-    music = fta.Audio()
-    play_pause_button = ft.Button(content = "Play", on_click = play_pause)
+    music = fta.Audio(src = "assets/audio/Sphere.mp3")
+    play_button = ft.Button(content = "Play", on_click = play)
+    pause_button = ft.Button(content = "Pause", on_click = pause)
+    resume_button = ft.Button(content = "resume", on_click = resume)
     next_song_button = ft.Button(content = "Next", on_click = next_song)
     previous_song_button = ft.Button(content = "Previous", on_click = previous_song)
 
     page.add(
         ft.Row(
-            controls = [play_pause_button, next_song_button, previous_song_button],
+            controls = [play_button, pause_button, next_song_button, previous_song_button],
             alignment = page.horizontal_alignment
         )
     )
